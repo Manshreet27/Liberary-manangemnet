46f76818def8acdaadcdbbe8bc8f4d8a36258671
manshreet27 <manshrit122@gmail.com>
2026-04-17 15:42:51 +0530

    2nd  push

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) throw new Error("MONGODB_URI not defined");
-
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export async function connectDB() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) throw new Error("MONGODB_URI not defined");
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
